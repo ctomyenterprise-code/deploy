217edb64bd154a3f39104e643de0cfc7da983f34
ctomyenterprise-code <cto.my.enterprise@gmail.com>
2026-06-09 15:18:45 -0400

    Actualizado Vercel

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-export default function handler(res,res) {
-    res.status(200).json({message:'Hola Backend desde vercel'});
+export default function handler(rer, res) {
+  res.status(200).json({ message: "Hola Backend desde vercel" });
 }
